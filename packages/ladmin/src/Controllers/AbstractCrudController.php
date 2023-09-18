e581cbaa1cbbb0eab2d7aa03d78e113872b7baf9
taiki-hashibe <tb.ba.700@gmail.com>
2023-09-18 14:15:09 +0900

    fix detail

--- packages/ladmin/src/Controllers/AbstractCrudController.php
+++ packages/ladmin/src/Controllers/AbstractCrudController.php
@@ -68,7 +68,6 @@ class AbstractCrudController extends Controller implements CrudControllerInterfa
     public function editor(Request $request): View
     {
         $fields = $this->editorFields();
-        dump($this->validationRules());
         $item = $this->crud->getQuery()->where($this->crud->getPrimaryKey(), $request->primaryKey)->first();
         if (!$item) {
             abort(404);
