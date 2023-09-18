bdef1a20e87dc8c1515ab89a1973a66278d7c97e
taiki-hashibe <tb.ba.700@gmail.com>
2023-09-18 23:45:18 +0900

    fix detail

--- packages/ladmin/resources/views/fields/editor/default.blade.php
+++ packages/ladmin/resources/views/fields/editor/default.blade.php
@@ -5,7 +5,8 @@
         @endif{{ $label }}
     </label>
     <input name="{{ $name }}" type="{{ isset($type) ? $type : 'text' }}"
-        class="block mt-1 px-2 py-2 w-full border focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm border-gray-300 @error($name) border-red-600 @enderror">
+        class="block mt-1 px-2 py-2 w-full border focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm border-gray-300 @error($name) border-red-600 @enderror"
+        value="{{ old($name) ? old($name) : $value }}">
     @error($name)
         <p class="text-sm text-red-600 space-y-1">
             {{ $message }}
