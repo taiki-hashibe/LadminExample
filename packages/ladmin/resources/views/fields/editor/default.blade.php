ae92aed3653aab9bb43527f3a13eaa41004ec465
taiki-hashibe <tb.ba.700@gmail.com>
2023-09-18 13:04:10 +0900

    fix detail

--- packages/ladmin/resources/views/fields/editor/default.blade.php
+++ packages/ladmin/resources/views/fields/editor/default.blade.php
@@ -1,5 +1,8 @@
 <div class="mb-3">
     <label for="{{ $name }}">{{ $label }}</label>
-    <input class="form-control" type="text" id="{{ $name }}" name="{{ $name }}"
-        value="{{ $value }}">
+    <input class="form-control @error($name) is-invalid @enderror" type="text" id="{{ $name }}"
+        name="{{ $name }}" value="{{ old($name) ? old($name) : $value }}">
+    @error($name)
+        <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
 </div>
