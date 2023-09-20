2622a3baa5bfe0c22a5ec191ac255607ee5a28cc
taiki-hashibe <tb.ba.700@gmail.com>
2023-09-20 16:07:44 +0900

    fix detai

--- packages/ladmin/resources/views/dashboard.blade.php
+++ packages/ladmin/resources/views/dashboard.blade.php
@@ -1,16 +1,5 @@
 <x-layouts-auth>
     <x-slot name="content">
-        <div class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ __('dashboard') }}
-            </div>
-        </div>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="p-4 mb-6 sm:p-8 bg-white shadow sm:rounded-lg">
-
-                </div>
-            </div>
-        </div>
+        <h1>{{ __('Dashboard') }}</h1>
     </x-slot>
 </x-layouts-auth>
--- packages/ladmin/src/Controllers/DashboardController.php
+++ packages/ladmin/src/Controllers/DashboardController.php
@@ -4,13 +4,11 @@ namespace LowB\Ladmin\Controllers;
 
 use Illuminate\Contracts\View\View as ViewView;
 use Illuminate\Support\Facades\View;
-use LowB\Ladmin\Config\Facades\LadminConfig;
 
 class DashboardController extends Controller
 {
     public function index(): ViewView
     {
-        dd('dashboard');
-        return View::first(['admin.dashboard', LadminConfig::theme() . 'dashboard']);
+        return View::first(['ladmin::dashboard']);
     }
 }
